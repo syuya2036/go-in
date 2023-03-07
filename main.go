333af545276237b0de1fd4288a453dfcdba554ae
syuya2036 <f20065@ichinoseki.kosen-ac.jp>
2023-03-07 07:55:23 +0000

    Part1 Complete

--- main.go
+++ main.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/syuya2036/go-in/repl"
+	"os"
+	"os/user"
 )
+
 func main() {
-fmt.Println("hello")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
--- token/token.go
+++ token/token.go
@@ -47,3 +47,21 @@ const (
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
 )
+
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
